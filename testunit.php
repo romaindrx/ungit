3dfe5ce8be458edfbe0654a287a66215c192e0e3
romaindrx <romaindrx@gmail.com>
2014-01-16 15:28:43 +0100

    test avec ajout de rand()

--- testunit.php
+++ testunit.php
@@ -1,22 +1,43 @@
-<p>Si plus petit c'est gagné...</p>
 
 
 <?php
 
 // Ajout d'un nombre aléatoire..entre 3 et 9
-//$roue = rand(3,9);
+$roue = rand(3,9);
 
-$roue = 7;
+//$roue = 7;
 
 $result = $roue + 2;
 
-echo $result."<br>";
 
+?>
+<!doctype html>
+<!--[if lte IE 7]> <html class="no-js ie67 ie678" lang="fr"> <![endif]-->
+<!--[if IE 8]> <html class="no-js ie8 ie678" lang="fr"> <![endif]-->
+<!--[if IE 9]> <html class="no-js ie9" lang="fr"> <![endif]-->
+<!--[if gt IE 9]> <!--><html class="no-js" lang="fr"> <!--<![endif]-->
+<head>
+		<meta charset="UTF-8">
+		<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+		<title>J'aime Schnaps.it</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!--[if lt IE 9]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link rel="stylesheet" href="css/knacss.css" media="all">
+		<link rel="stylesheet" href="css/styles.css" media="all">
+</head>
+<body>
+<p>Si plus petit c'est gagné...</p>
+	<p><?php echo $result."<br>"; ?></p>
 
+<?php
 if ($result < 6){
-	echo "Le résultat est plus petit";
+	echo "<p>Le résultat est plus petit</p>";
 }
 else{
-	echo "dommage c'est plus grand...";	
+	echo "<p>dommage c'est plus grand...</p>";	
 }
 ?>
+</body>
+</html>
